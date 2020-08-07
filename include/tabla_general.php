ff175974e1b75dae8932910fce5893eab337f6ae
FcoSanchez <f.sanchez@solucionessl.com>
2020-08-07 18:41:27 +0200

    ADD - Rediseño elementos includes

--- include/tabla_general.php
+++ include/tabla_general.php
@@ -1,23 +1,26 @@
 <?php
+// cambios
 require_once("../include/session.php");
-$where_c_coste=" id_c_coste={$_SESSION['id_c_coste']} " ;   // AND $where_c_coste 
+$where_c_coste = " id_c_coste={$_SESSION['id_c_coste']} ";
+$id_c_coste = $_SESSION['id_c_coste'];
+
+$titulo = 'Tabla General';
+
+//INICIO
+include_once('../templates/_inc_privado1_header.php');
+include_once('../templates/_inc_privado2_navbar.php');
+
 ?>
-<HTML>
-<HEAD>
-    <TITLE>Tabla General</TITLE>
-	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
-	
-	<link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-	
-  <!--ANULADO 16JUNIO20<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">-->
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-   <link rel="stylesheet" href="../css/estilos.css<?php echo (isset($_SESSION["is_desarrollo"]) AND $_SESSION["is_desarrollo"])? "?d=".date("ts") : "" ; ?>" type="text/css">
 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <!--ANULADO 16JUNIO20<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
-</HEAD>
-<BODY>
+        <!-- Contenido principal -->
+        <div class="container-fluid bg-light">
+            <div class="row">
+                <!--****************** ESPACIO LATERAL  *****************-->
+                <div class="col-12 col-md-4 col-lg-3"></div>
+                <!--****************** ESPACIO LATERAL  *****************-->
 
+                <!--****************** BUSQUEDA GLOBAL  *****************-->
+                <div class="col-12 col-md-4 col-lg-9">
  
 
 
@@ -25,10 +28,6 @@ $where_c_coste=" id_c_coste={$_SESSION['id_c_coste']} " ;   // AND $where_c_cost
 <?php // $id_cta_banco=$_GET["id_cta_banco"];?>
 
 <!-- CONEXION CON LA BBDD Y MENUS -->
-<?php require_once("../../conexion.php"); ?> 
-<?php require_once("../include/funciones.php"); ?> 
-<?php require_once("../menu/topbar.php");?>
-
 
 <div style="overflow:visible">	   
    
@@ -161,8 +160,13 @@ $Conn->close();
 </div>
 
 	
-	
-<?php require '../include/footer.php'; ?>
-</BODY>
-</HTML>
+                </div>
+                <!--****************** BUSQUEDA GLOBAL  *****************-->
+            </div>
+        </div>
+        <!-- FIN Contenido principal -->
 
+<?php 
+
+//FIN
+include_once('../templates/_inc_privado3_footer.php');
