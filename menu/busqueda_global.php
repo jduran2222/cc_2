48cb74fa163dbb5d8954ed93c83123c606d8fbb4
jduran2222 <juanduran@ingenop.com>
2021-01-15 17:20:15 +0100

    EDIT prueba busqueda_global.php version 2 Github

--- menu/busqueda_global.php
+++ menu/busqueda_global.php
@@ -6,7 +6,7 @@ $id_c_coste = $_SESSION['id_c_coste'];
 
 $titulo = 'Búsqueda Global';
 
-// PRUEBA DE GITHUB, JUAND, ENERO2021
+// PRUEBA DE GITHUB, JUAND, ENERO2021, VERSION 2 GITHUB
 
 //INICIO
 include_once('../templates/_inc_privado1_header.php');
